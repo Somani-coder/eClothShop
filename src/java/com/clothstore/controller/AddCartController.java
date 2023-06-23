88337e0a6d68bf74518d97cebba2c517dc31e84b
Somani-coder <lipsitapanda219@gmail.com>
2023-06-23 18:15:10 +0530

    product save to db.

---
+++ src/java/com/clothstore/controller/AddCartController.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clothstore.controller;
+
+import com.clothstore.model.Product;
+import com.clothstore.model.User;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+
+/**
+ *
+ * @author LIPSITA
+ */
+public class AddCartController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+        
+        } BufferedReader br
+                    = new BufferedReader(new InputStreamReader(request.getInputStream()));
+
+            String json = "";
+            if (br != null) {
+                json = br.readLine();
+                System.out.println(json);
+            }
+
+            // 2. initiate jackson mapper
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode jsonNode = mapper.readTree(json);
+            String command = jsonNode.get("action").asText();
+            JsonNode products = jsonNode.get("products");
+            
+            
+
+            // 3. Convert received JSON to Article
+            Product product = mapper.readValue(products, Product.class);
+                   // (products, Product.class);
+
+            // 4. Set response type to JSON
+            response.setContentType("application/json");
+          //  String un = user.getuName();
+            //String up = user.getuPass();
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/com/clothstore/model/Product.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clothstore.model;
+
+/**
+ *
+ * @author LIPSITA
+ */
+public class Product {
+
+    private int productId;
+    private String description;
+    private String size;
+    private int price;
+    private int quantity;
+    private String source;
+
+}
